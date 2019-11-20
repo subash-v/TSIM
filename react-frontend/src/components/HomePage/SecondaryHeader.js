1a3d6900b11b4bb8798021cdd31204355ba0f6bb
banupriya <banupriya@xelpmoc.in>
2019-11-20 18:50:22 +0530

    design changed in header

--- react-frontend/src/components/HomePage/SecondaryHeader.js
+++ react-frontend/src/components/HomePage/SecondaryHeader.js
@@ -7,43 +7,41 @@ import Icon from "../../core/Icon";
 export default class SecondaryHeader extends Component {
   render() {
     return (
-      <CenteredContent>
-        <div className={styles.headerBase}>
-          <div className={styles.headerHolder}>
-            <div className={styles.logoHolder}>
-              <div className={styles.logo}>The star in me</div>
+      <div className={styles.headerBase}>
+        <div className={styles.headerHolder}>
+          <div className={styles.logoHolder}>
+            <div className={styles.logo}>The star in me</div>
+          </div>
+          <div className={styles.searchLoginHolder}>
+            <div className={styles.search}>
+              <Icon image={search} size={"20px"}></Icon>
             </div>
-            <div className={styles.searchLoginHolder}>
-              <div className={styles.search}>
-                <Icon image={search} size={"20px"}></Icon>
-              </div>
 
-              <div className={styles.buttonHolder}>
-                <div className={styles.login}>
-                  <Button
-                    type="secondary"
-                    height={40}
-                    width={120}
-                    fontColor={"#FFF"}
-                    label="LOGIN"
-                    borderColor="none"
-                  />
-                </div>
-                <div className={styles.signup}>
-                  <Button
-                    type="primary"
-                    backgroundColor={"#fff"}
-                    fontColor={"#4F439A"}
-                    height={40}
-                    width={120}
-                    label="SIGN UP"
-                  />
-                </div>
+            <div className={styles.buttonHolder}>
+              <div className={styles.login}>
+                <Button
+                  type="secondary"
+                  height={40}
+                  width={120}
+                  fontColor={"#FFF"}
+                  label="LOGIN"
+                  borderColor="none"
+                />
+              </div>
+              <div className={styles.signup}>
+                <Button
+                  type="primary"
+                  backgroundColor={"#fff"}
+                  fontColor={"#4F439A"}
+                  height={40}
+                  width={120}
+                  label="SIGN UP"
+                />
               </div>
             </div>
           </div>
         </div>
-      </CenteredContent>
+      </div>
     );
   }
 }
