210b3b7a4720f65cd90eabed8a45f5ac7d7b8672
subash-v <subash.v@xelpmoc.in>
2019-11-25 19:08:25 +0530

    feed page

--- react-frontend/src/components/Feed/Feed.js
+++ react-frontend/src/components/Feed/Feed.js
@@ -4,6 +4,13 @@ import CenteredContent from "../../core/CenteredContent";
 import profileImage from "../Images/b.jpg";
 import Image from "../../core/Image";
 import VerticalStatus from "./VerticalStatus";
+import Button from "../general/Button";
+import PrimaryHeaderContainer from "../HomePage/container/PrimaryHeaderContainer";
+import ProfileDetailsInFeed from "./ProfileDetailsInFeed";
+import ProfileConnection from "./ProfileConnection";
+import AddMoreIntrest from "./AddMoreIntrest";
+import GuidesRecommended from "./GuidesRecommended";
+
 const data = [
   {
     data: profileImage
@@ -22,31 +29,52 @@ export default class Feed extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      position: 0
+      position: 0,
+      guidePosition: 0
     };
   }
-  goForward = () => {
-    if (this.state.position < data.length - 1) {
-      this.setState({ position: this.state.position + 1 });
+  goForward = val => {
+    if (val !== "guide") {
+      if (this.state.position < data.length - 1) {
+        this.setState({ position: this.state.position + 1 });
+      }
+    } else {
+      if (this.state.guidePosition < data.length - 1) {
+        this.setState({ guidePosition: this.state.guidePosition + 1 });
+      }
     }
   };
-  goBack = () => {
-    if (this.state.position > 0) {
-      this.setState({ position: this.state.position - 1 });
+  goBack = val => {
+    if (val !== "guide") {
+      if (this.state.position > 0) {
+        this.setState({ position: this.state.position - 1 });
+      }
+    } else {
+      if (this.state.guidePosition > 0) {
+        this.setState({ guidePosition: this.state.guidePosition - 1 });
+      }
     }
   };
   render() {
     const translationAmount = -(100 * this.state.position);
     const transform = `translateX(${translationAmount}%)`;
+    const translationAmount1 = -(100 * this.state.guidePosition);
+    const guideTransform = `translateX(${translationAmount1}%)`;
     const style = {
       transform: transform
     };
+    const guideStyle = {
+      transform: guideTransform
+    };
     return (
-      <div>
-        <div className={styles.indicatorHolder}>
+      <div className={styles.base}>
+        {/* <div className={styles.indicatorHolder}>
           <VerticalStatus first="complete" second="active" third="incompele" />
-        </div>
+        </div> */}
+
+        <PrimaryHeaderContainer history={this.props.history} />
         <CenteredContent>
+          <div className={styles.heading}>Top Picks</div>
           <div className={styles.imageDisplaySection}>
             <div className={styles.leftImage}>
               <div className={styles.leftImgCont}>
@@ -54,8 +82,36 @@ export default class Feed extends Component {
                   src={profileImage}
                   width="100%"
                   height="100%"
-                  className={styles.image}
+                  className={styles.imageEventBanner}
                 />
+                <div className={styles.overlay}>
+                  <div className={styles.bannerEventDetailsContainer}>
+                    <div className={styles.bannerEventDetails}>
+                      <div className={styles.tagContainer}>
+                        <div className={styles.tagButton}>EVENT</div>
+                      </div>
+                      <div className={styles.eventHeading}>
+                        Work Life Balance - Learn what makes a successful one
+                      </div>
+                      <div className={styles.dateAndPlaceContainer}>
+                        3rd November 2019 <span className={styles.dot} /> Reva
+                        University
+                      </div>
+                      <div className={styles.viewEventButtonContainer}>
+                        <Button
+                          type="primary"
+                          backgroundColor={"rgba(255,255,255,0.5"}
+                          fontColor={"#fff"}
+                          borderColor="rgba(255,255,255,0.5)"
+                          height={50}
+                          width={146}
+                          label="VIEW EVENT"
+                          borderRadius={10}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
             <div className={styles.rightImageHolder}>
@@ -66,6 +122,43 @@ export default class Feed extends Component {
                   height="100%"
                   className={styles.image}
                 />
+                <div className={styles.overlay}>
+                  <div className={styles.bannerStoryContainer}>
+                    <div className={styles.bannerEventDetails}>
+                      <div className={styles.tagContainer}>
+                        <div className={styles.tagButton}>STORIES</div>
+                      </div>
+                      <div className={styles.storiesHeading}>
+                        Why “Me time” is essential
+                      </div>
+                      <div className={styles.dateAndPlaceContainer}>
+                        Uma Kasoji <span className={styles.dot} /> 3 Mins
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.smallImage}>
+                <div className={styles.popularQuestionContainer}>
+                  <div className={styles.popularQuestionHeading}>
+                    POPULAR QUESTION
+                  </div>
+                  <div className={styles.question}>
+                    What are the most interesting facts about human behavior?
+                  </div>
+                  <div className={styles.answerButton}>
+                    <Button
+                      type="primary"
+                      backgroundColor={"#fff"}
+                      fontColor={"#4F439A"}
+                      borderColor={"#fff"}
+                      height={30}
+                      width={90}
+                      label="ANSWER"
+                      borderRadius={3}
+                    />
+                  </div>
+                </div>
               </div>
               <div className={styles.smallImage}>
                 <img
@@ -74,57 +167,161 @@ export default class Feed extends Component {
                   height="100%"
                   className={styles.image}
                 />
+                <div className={styles.overlay}>
+                  <div className={styles.bannerStoryContainer}>
+                    <div className={styles.bannerEventDetails}>
+                      <div className={styles.tagContainer}>
+                        <div className={styles.tagButton}>STORIES</div>
+                      </div>
+                      <div className={styles.storiesHeading}>
+                        Why “Me time” is essential
+                      </div>
+                      <div className={styles.dateAndPlaceContainer}>
+                        Uma Kasoji <span className={styles.dot} /> 3 Mins
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
               <div className={styles.smallImage}>
-                <img
-                  src={profileImage}
-                  width="100%"
-                  height="100%"
-                  className={styles.image}
-                />
-              </div>
-              <div className={styles.smallImage}>
-                <img
-                  src={profileImage}
-                  width="100%"
-                  height="100%"
-                  className={styles.image}
-                />
+                <div className={styles.connectProfileContainer}>
+                  <div className={styles.coverImageForConnectionProfile}>
+                    <img
+                      src={profileImage}
+                      width="100%"
+                      height="100%"
+                      className={styles.image}
+                    />
+                  </div>
+                  <div className={styles.connectProfileHolder}>
+                    <div className={styles.connectProfileImageHolder}>
+                      <img
+                        src={profileImage}
+                        width="100%"
+                        height="100%"
+                        className={styles.connectProfileImage}
+                      />
+                    </div>
+                    <div className={styles.profileName}>Akarshika Das</div>
+                  </div>
+                  <div className={styles.connectProfileDetailsContainer}>
+                    <div className={styles.connectProfileDetails}>
+                      <div className={styles.connectProfileDesignations}>
+                        Life Coach | Product Designer
+                      </div>
+                      <div className={styles.connectProfileTags}>
+                        Product Management{" "}
+                        <span className={styles.connectProfileTagsMoreThenOne}>
+                          +2
+                        </span>
+                      </div>
+                    </div>
+                    <div>
+                      <Button
+                        type="primary"
+                        backgroundColor={"#E0DEED"}
+                        fontColor={"#4F439A"}
+                        borderColor={"#E0DEED"}
+                        height={30}
+                        width={90}
+                        label="CONNECT"
+                        borderRadius={3}
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
           <div className={styles.dataHolderWrapper}>
             <div className={styles.leftSidePannel}>
-              <div className={styles.dataDiv}>john</div>
-              <div className={styles.dataDiv}>priya</div>
-              <div className={styles.dataDiv}>john-priya</div>
-              <div className={styles.dataDiv}>john</div>
+              <div className={styles.myProfileSection}>
+                <ProfileDetailsInFeed />
+              </div>
+              <div className={styles.addMoreIntrestContainer}>
+                <AddMoreIntrest />
+              </div>
+              {/* <div className={styles.dataDiv}>john-priya</div>
+              <div className={styles.dataDiv}>john</div> */}
             </div>
             <div className={styles.rightSidePannel}>
+              <div className={styles.profileConnectionHeading}>
+                Coaches recommended for you
+              </div>
               <div className={styles.caurosel}>
                 <div className={styles.slider} style={style}>
                   {data.map((val, i) => {
                     return (
                       <div className={styles.cauroselData}>
-                        <Image image={val.data} />
+                        <ProfileConnection />
                       </div>
                     );
                   })}
                 </div>
               </div>
-              <div className={styles.forwrdBack}>
-                <div className={styles.back} onClick={() => this.goBack()}>
-                  {" "}
-                  {"<"}{" "}
+              <div className={styles.forwrdBackContainer}>
+                <div className={styles.forwrdBack}>
+                  <div
+                    className={styles.back}
+                    onClick={() => this.goBack()}
+                  ></div>
+                  <div
+                    className={styles.forward}
+                    onClick={() => this.goForward()}
+                  ></div>
                 </div>
-                <div
-                  className={styles.forward}
-                  onClick={() => this.goForward()}
-                >
-                  >
+                <div className={styles.viewAllButtonContainer}>
+                  <Button
+                    type="primary"
+                    backgroundColor={"transparent"}
+                    fontColor={"#1A1A1A"}
+                    borderColor={"#7F7F7F"}
+                    height={30}
+                    width={116}
+                    label="VIEW ALL"
+                    borderRadius={5}
+                  />
                 </div>
               </div>
-              <div className={styles.dataDiv}>john</div>
+              <div className={styles.guideRecommendedHeading}>
+                Guides recommended for you
+              </div>
+              <div className={styles.caurosel}>
+                <div className={styles.slider} style={guideStyle}>
+                  {data.map((val, i) => {
+                    return (
+                      <div className={styles.cauroselData}>
+                        <GuidesRecommended />
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className={styles.forwrdBackContainer}>
+                <div className={styles.forwrdBack}>
+                  <div
+                    className={styles.back}
+                    onClick={() => this.goBack("guide")}
+                  ></div>
+                  <div
+                    className={styles.forward}
+                    onClick={() => this.goForward("guide")}
+                  ></div>
+                </div>
+                <div className={styles.viewAllButtonContainer}>
+                  <Button
+                    type="primary"
+                    backgroundColor={"transparent"}
+                    fontColor={"#1A1A1A"}
+                    borderColor={"#7F7F7F"}
+                    height={30}
+                    width={116}
+                    label="VIEW ALL"
+                    borderRadius={5}
+                  />
+                </div>
+              </div>
+
               <div className={styles.dataDiv}>priya</div>
               <div className={styles.dataDiv}>john-priya</div>
             </div>
---
+++ react-frontend/src/components/Feed/GuidesRecommended.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import styles from "./GuidesRecommended.css";
+export default class GuidesRecommended extends Component {
+  render() {
+    return <div className={styles.base}>GuidesRecommended</div>;
+  }
+}
