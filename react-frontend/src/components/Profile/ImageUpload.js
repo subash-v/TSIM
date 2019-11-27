a9acfc0781021676d1c11ca7c928da92304a07f2
akash-pandey-xelp <akash.pandey@xelpmoc.in>
2019-11-27 15:33:10 +0530

    image Upload functionality

--- react-frontend/src/components/Profile/ImageUpload.js
+++ react-frontend/src/components/Profile/ImageUpload.js
@@ -1,19 +1,41 @@
 import React, { Component } from "react";
 import styles from "./ImageUpload.css";
 import BottomSlideModal from "../modules/component/BottomSlideModal";
-
+import ReactCrop from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
 class ImageUplaod extends Component {
   constructor(props) {
     super(props);
     this.state = {
       file: [],
       slideIndex: 0,
-      maxSlide: 5
+      maxSlide: 5,
+      crop: {
+        aspect: 1 / 1
+      }
     };
   }
 
   onChange = async event => {
     await this.setState({ file: [...this.state.file, ...event.target.files] });
+  };
+
+  handleOnCrop = crop => {
+    console.log(crop);
+    this.setState({
+      crop
+    });
+    console.log(this.state);
+  };
+
+  removeImageFromList = val => {
+    console.log(this.state.file);
+    let file = this.state.file;
+    let newarray = file.splice(0, val);
+    this.setState({
+      file: newarray
+    });
+    console.log(this.state.file);
   };
 
   render() {
@@ -24,9 +46,8 @@ class ImageUplaod extends Component {
           <div style={{ position: "relative" }}>
             <div className={styles.uploadImageSection}>
               {this.state.file.length > 0 ? (
-                <div>
-                  <div className={styles.mySlides}>
-                    <div
+                <div className={styles.mySlides}>
+                  {/* <div
                       style={{
                         backgroundImage: `url(${URL.createObjectURL(
                           this.state.file[0]
@@ -35,67 +56,15 @@ class ImageUplaod extends Component {
                         backgroundPosition: "10% 20%",
                         backgroundSize: "90%"
                       }}
-                      // src={image0}
                       className={styles.homepageCarousal}
-                    />
-                  </div>
-
-                  {/* <div className={styles.mySlides}>
-                    <div
-                      style={{
-                        // backgroundImage: `url(${URL.createObjectURL(
-                        //   this.state.file[1]
-                        // )})`,
-                        backgroundRepeat: "no-repeat",
-                        backgroundPosition: "10% 20%",
-                        backgroundSize: "90%"
-                      }}
-                      // src={image0}
-                      className={styles.homepageCarousal}
-                    />
-                  </div>
-                  <div className={styles.mySlides}>
-                    <div
-                      style={{
-                        // backgroundImage: `url(${URL.createObjectURL(
-                        //   this.state.file[2]
-                        // )})`,
-                        backgroundRepeat: "no-repeat",
-                        backgroundPosition: "10% 20%",
-                        backgroundSize: "90%"
-                      }}
-                      // src={image0}
-                      className={styles.homepageCarousal}
-                    />
-                  </div>
-                  <div className={styles.mySlides}>
-                    <div
-                      style={{
-                        // backgroundImage: `url(${URL.createObjectURL(
-                        //   this.state.file[3]
-                        // )})`,
-                        backgroundRepeat: "no-repeat",
-                        backgroundPosition: "10% 20%",
-                        backgroundSize: "90%"
-                      }}
-                      // src={image0}
-                      className={styles.homepageCarousal}
-                    />
-                  </div>
-                  <div className={styles.mySlides}>
-                    <div
-                      style={{
-                        // backgroundImage: `url(${URL.createObjectURL(
-                        //   this.state.file[4]
-                        // )})`,
-                        backgroundRepeat: "no-repeat",
-                        backgroundPosition: "10% 20%",
-                        backgroundSize: "90%"
-                      }}
-                      // src={image0}
-                      className={styles.homepageCarousal}
-                    />
-                  </div> */}
+                    /> */}
+                  <ReactCrop
+                    src={URL.createObjectURL(this.state.file[0])}
+                    crop={this.state.crop}
+                    width={"500px"}
+                    height={"500px"}
+                    // onChange={this.handleOnCrop  }
+                  />
                 </div>
               ) : (
                 <div className={styles.uploadImageDummy} />
@@ -103,8 +72,8 @@ class ImageUplaod extends Component {
             </div>
             <div className={styles.uploadImageGroup}>
               {" "}
-              <div className={styles.uploadPlusButton}>
-                {this.state.file[0] < 0 ? (
+              {!this.state.file[0] ? (
+                <div className={styles.uploadPlusButton}>
                   <input
                     type="file"
                     accept={"image"}
@@ -118,19 +87,25 @@ class ImageUplaod extends Component {
                     }}
                     value={this.value}
                   />
-                ) : (
-                  <div className={styles.imageWithCross}>
-                    <div
-                      style={{
-                        backgroundImage: `url(${URL.createObjectURL(
-                          this.state.file[0]
-                        )})`
-                      }}
-                    ></div>
-                    <div className={styles.crossButton}></div>
-                  </div>
-                )}
-              </div>
+                </div>
+              ) : (
+                <div
+                  className={styles.imageWithCross}
+                  style={{
+                    backgroundImage: `url(${URL.createObjectURL(
+                      this.state.file[0]
+                    )})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "70%",
+                    backgroundPosition: "center"
+                  }}
+                >
+                  <div
+                    className={styles.crossButton}
+                    onClick={() => this.removeImageFromList(0)}
+                  ></div>
+                </div>
+              )}
               <div className={styles.uploadPlusButton}>
                 <input
                   type="file"
