2325cb42730c6fe29d29b9a4c1720be8e4f7bfb0
banupriya <banupriya@xelpmoc.in>
2019-11-29 15:16:16 +0530

    added responsiveness for modules

--- react-frontend/src/components/HomePage/MorePage.js
+++ react-frontend/src/components/HomePage/MorePage.js
@@ -4,6 +4,8 @@ import landingImage from "../../images/landing-page-Event.svg";
 import Icon from "../../core/Icon";
 import CenteredContent from "../../core/CenteredContent";
 import PrimaryHeaderContainer from "./container/PrimaryHeaderContainer";
+import DesktopOnly from "../general/DesktopOnly";
+import MobileOnly from "../general/MobileOnly";
 const more = [
   { data: landingImage, name: "EVENTS" },
   { name: "RESOURCES" },
@@ -27,7 +29,12 @@ export default class MorePage extends Component {
                 return (
                   <div className={styles.border}>
                     <div className={styles.linkText}>
-                      <Icon image={landingImage} size={50} />
+                      <DesktopOnly>
+                        <Icon image={landingImage} size={50} />
+                      </DesktopOnly>
+                      <MobileOnly>
+                        <Icon image={landingImage} size={30} />
+                      </MobileOnly>
                     </div>
                     <div className={styles.label}>{value.name}</div>
                   </div>
